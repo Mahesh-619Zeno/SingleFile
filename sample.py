84c7f81045fbefb2125ceda40a868724fee50a64
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-07-30 21:40:26 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,19 +7,19 @@
 number = 29
 is_condition_met = False
 
-if num == 0 or num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
+if number == 0 or number == 1:
+    print(number, "is not a prime number")
+elif number > 1:
     # check for factors
-    for i in range(2, num):
-        if (num % i) == 0:
+    for i in range(2, number):
+        if (number % i) == 0:
             # if factor is found, set flag to True
             flag = True
             # break out of loop
             break
 
     # check if flag is True
-    if flag:
-        print(num, "is not a prime number")
+    if is_condition_met:
+        print(number, "is not a prime number")
     else:
-        print(num, "is a prime number")
+        print(number, "is a prime number")
