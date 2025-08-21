062ad7824496fdadff88817fc9cdabcf4ed6e06a
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-08-21 13:24:56 +0530

    feat: uncommented

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ if number == 0 or number == 1:
     print(number, "is not a prime number")
 elif number > 1:
     # check for factors
-    """for i in range(2, number):
+    for i in range(2, number):
         if (number % i) == 0:
             # if factor is found, set flag to True
             flag = True
@@ -17,4 +17,4 @@ elif number > 1:
     if is_condition_met:
         print(number, "is not a prime number")
     else:
-        print(number, "is a prime number")"""
+        print(number, "is a prime number")
